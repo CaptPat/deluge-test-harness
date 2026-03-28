bca83e05b6335de624b15ea9944de12f53394abe
CaptPat <17856081+CaptPat@users.noreply.github.com>
2026-03-28 12:48:20 -0400

    chore: remove accidentally committed build-utest from tracking

--- build-utest/_deps/cpputest-build/src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: P:/VSCodeDesktop/projects/deluge-test-harness/build-utest/_deps/cpputest-src/src
-# Build directory: P:/VSCodeDesktop/projects/deluge-test-harness/build-utest/_deps/cpputest-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("CppUTest")
-subdirs("CppUTestExt")
